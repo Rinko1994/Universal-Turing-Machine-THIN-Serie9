e8dadd198273164ee090a2ba7a860e638f219251
maant001 <71355873+maant001@users.noreply.github.com>
2021-05-09 02:20:06 +0200

    - some changes LOL

--- src/main/java/TuringMachine/TuringMachine.java
+++ src/main/java/TuringMachine/TuringMachine.java
@@ -1,5 +1,15 @@
 package TuringMachine;
 
 public abstract class TuringMachine {
+    private State startingState;
+    private Head head;
+
+    private boolean stepMode;
+
+    public TuringMachine(State startingState, int number1, int number2, boolean stepMode) {
+        this.startingState = startingState;
+        this.head = new Head(number1, number2);
+        this.stepMode = stepMode;
+    }
 
 }
