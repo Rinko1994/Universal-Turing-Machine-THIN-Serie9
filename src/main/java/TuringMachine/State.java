0f530e96435e7fa04627974710513220635fdd32
maant001 <71355873+maant001@users.noreply.github.com>
2021-05-09 02:03:12 +0200

    - some changes LOL

--- src/main/java/TuringMachine/State.java
+++ src/main/java/TuringMachine/State.java
@@ -1,6 +1,7 @@
 package TuringMachine;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class State {
@@ -8,5 +9,30 @@ public class State {
     private boolean acceptedState;
     private List<Transferfunction> transferfunctionList;
 
+    public State(String name, boolean acceptedState) {
+        this.name = name;
+        this.acceptedState = acceptedState;
+        this.transferfunctionList = new ArrayList<Transferfunction>();
+    }
 
+    public boolean acceptedState() {
+        return acceptedState;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Transferfunction getNextTransferFunction(String readSymbol) {
+        for (Transferfunction element : transferfunctionList) {
+            if (element.getReadSymbol().equals(readSymbol)) {
+                return element;
+            }
+        }
+        throw new IllegalArgumentException("Illegal language");
+    }
+
+    public void addTransferfunction(Transferfunction tf) {
+        transferfunctionList.add(tf);
+    }
 }
